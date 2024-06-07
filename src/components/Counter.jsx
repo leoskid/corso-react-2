7a8c4a891b8154ac0f527fee7944c53937709ebd
leoskid <leonardo.panzanaro@icloud.com>
2024-06-07 08:44:02 +0200

    primo commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function Counter() {
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<button onClick={() => setCount((count) => count + 1)}>
+				Hai cliccato {count} volte
+			</button>
+		</div>
+	);
+}
+
+export default Counter;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Benvenuto(props) {
+	return <h1>Ciao! Benvenuto {props.nome}</h1>;
+}
+
+export default Benvenuto;
