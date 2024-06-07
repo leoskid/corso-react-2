e1cd7932b4d994ac87d65baf0412bed8d193f1af
leoskid <leonardo.panzanaro@icloud.com>
2024-06-07 09:53:39 +0200

    modifiche

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Benvenuto(props) {
-	return <h1>Ciao! Benvenuto {props.nome}</h1>;
+	return <h1>Ciao! Benvenuto {props.nome} !</h1>;
 }
 
 export default Benvenuto;
